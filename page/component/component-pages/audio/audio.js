c34f70b7545cde244a98c340854df62b49eac1cf
luoyu <272864318@qq.com>
2016-10-14 23:42:17 +0800

    first commit

---
+++ page/component/component-pages/audio/audio.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    current: {
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻',
+      author: '许巍',
+    },
+    audioAction: {
+      method: 'pause'
+    }
+  },
+  audioPlayed: function(e) {
+    console.log('audio is played')
+  }
+})
---
+++ page/index.js
@@ -0,0 +1,62 @@
+Page({
+  data: {
+    bannerUrls: [
+      'http://service.ipinbb.com:8080/ImageService/GetImage?imageName=750--300__group1__$$M00$$00$$35$$CgqAy1gARE6ATVW_AATVFxSqnd0643.png',
+      'http://service.ipinbb.com:8080/ImageService/GetImage?imageName=750--300__group1__$$M00$$00$$35$$CgoSrFgARF-AVp1FAAVmt1Ye8gk871.png',
+      'http://service.ipinbb.com:8080/ImageService/GetImage?imageName=750--300__group1__$$M00$$00$$35$$CgqAy1gARIyAadf0AAUPDtnD8xU586.png'
+    ],
+    bannerConfig: {
+      mode: "aspectFit",
+      indicatorDots: true,
+      autoplay: true,
+      interval: 3000,
+      duration: 500,
+    },
+    activityUrls: [
+      'http://m.ipinbb.com/ipbb/static/images/home-01.png',
+      "http://m.ipinbb.com/ipbb/static/images/home-02.png",
+      "http://m.ipinbb.com/ipbb/static/images/home-03.png",
+      "http://m.ipinbb.com/ipbb/static/images/home-04.png"
+    ],
+    navItems: [{
+      text: "最新",
+      className: "navigator-hover",
+      url: ""
+    },{
+      text: "全网折上折",
+      className: "",
+      url: ""
+    },{
+      text: "儿童玩具",
+      className: "",
+      url: ""
+    },{
+      text: "运动修身",
+      className: "",
+      url: ""
+    },{
+      text: "学习用品",
+      className: "",
+      url: ""
+    },{
+      text: "童装童鞋",
+      className: "",
+      url: ""
+    },{
+      text: "孕童用品",
+      className: "",
+      url: ""
+    },{
+      text: "孕童用品",
+      className: "",
+      url: ""
+    }],
+    list: [{
+
+    },{
+
+    }]
+  }  
+})
+
+console.log(window);
