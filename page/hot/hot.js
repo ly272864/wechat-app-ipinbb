91d33727d43a7f1e8bbdbca0f5d2a30fc641296d
luoyu <272864318@qq.com>
2016-10-28 19:52:31 +0800

    热拼页面修改

--- page/hot/hot.js
+++ page/hot/hot.js
@@ -1,102 +1,129 @@
 Page({
     data: {
-        hidden: true,
-        lists: [{
-            id: "1456",
-            productImg: "http://img.alicdn.com/imgextra/i3/TB1jquENpXXXXXOXFXXXXXXXXXX_!!0-item_pic.jpg",
-            title: "2人团-七匹狼童装2016秋冬儿童毛衣",
-            people: "2",
-            price: "77.40",
-            lack: "1",
-            userImg: [
-                "http://wx.qlogo.cn/mmopen/rVfOAgXaZicbsnzaU31hjlFXJn8Es742V0myHhOFsibtWqSibREIPx8JQdI2Ce0zVBQOJUicANHRsZib6ZQTxqkcxBdSRomgXSpOD/0"
-            ]
-        },{
-            id: "1456",
-            productImg: "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=800--800__group1__$$M00$$00$$35$$CgoSrFf92aqAZuS1AAD8G_1ckwE719.jpg",
-            title: "6人团-纯棉加绒加厚保暖背心吊带",
-            people: "6",
-            price: "9.80",
-            lack: "1",
-            userImg: [
-                "http://wx.qlogo.cn/mmopen/Q3auHgzwzM7wXR6Iw880XVEjjwF3gVp3MW8XksjDF1cQQ4YlGlZI4I3MKia6kkZHHvaRtl3licXdZTNIB34aSxZMtdDcqW7t40E8tP2y5y8wY/0",                
-                "http://wx.qlogo.cn/mmopen/XuNnRjxJia467j0VjmneIjOUyxdpvK7J0FWuunWhCpQibhR6lVoQmpYg1KQ2qQEic6N78sJGLfKMLQP0mJcFFoftWyM8G3iafTDe/0",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgqAy1d17JuAEueWAAAemM4_igk664.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgoSrFd112-AMMWQAAAzkLsDE4k917.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=1024--1024__group1__$$M00$$00$$21$$CgqAy1d17M6AUBRoAAGWfUdODeU139.jpg"
-            ]
-        },{
-            id: "1456",
-            productImg: "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=400--400__group1__$$M00$$00$$31$$CgqAy1fPzLuAGufOAADgIufXFN8825.jpg",
-            title: "6人团-珊瑚绒加厚款粘贴式马桶垫坐垫",
-            people: "6",
-            price: "6.80",
-            lack: "2",
-            userImg: [
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgoSrFd11myADovRAAAs3D7vCu8280.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=230--210__group1__$$M00$$00$$22$$CgoSrFd2DmmACUJTAAEKvEuROdk254.jpg",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$1B$$CgqAy1dw3i-ANHSwAAJ5EpswwIE098.png",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$22$$CgoSrFd2DoSAWy1rAAAriozftjA067.jpg"
-            ]
-        },{
-            id: "1456",
-            productImg: "http://img.alicdn.com/imgextra/i4/2634245589/TB2dLtwiVXXXXaTXpXXXXXXXXXX_!!2634245589.jpg",
-            title: "6人团-幼儿12册新编描红练习册",
-            people: "6",
-            price: "9.80",
-            lack: "2",
-            userImg: [
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgqAy1d12HSASZpYAAA_q1i0pzk046.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$1B$$CgoSrFdw6DmAK3jHAAAfIZizQMc568.jpg",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$22$$CgoSrFd17juAaE7cAAB7DK-K1RM122.jpg",
-                "http://wx.qlogo.cn/mmopen/XuNnRjxJia467j0VjmneIjCHfIW3xaOVFuIQfCYGBybGQqN4nayBiaMN8iaxdpmfGiafW84R2aIQYRl7Wn2F0Da971BSlf76Q6ud/0"
-            ]
-        },{
-            id: "1456",
-            productImg: "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=800--800__group1__$$M00$$00$$35$$CgoSrFf92aqAZuS1AAD8G_1ckwE719.jpg",
-            title: "6人团-纯棉加绒加厚保暖背心吊带",
-            people: "6",
-            price: "9.80",
-            lack: "1",
-            userImg: [
-                "http://wx.qlogo.cn/mmopen/Q3auHgzwzM7wXR6Iw880XVEjjwF3gVp3MW8XksjDF1cQQ4YlGlZI4I3MKia6kkZHHvaRtl3licXdZTNIB34aSxZMtdDcqW7t40E8tP2y5y8wY/0",                
-                "http://wx.qlogo.cn/mmopen/XuNnRjxJia467j0VjmneIjOUyxdpvK7J0FWuunWhCpQibhR6lVoQmpYg1KQ2qQEic6N78sJGLfKMLQP0mJcFFoftWyM8G3iafTDe/0",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgqAy1d17JuAEueWAAAemM4_igk664.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgoSrFd112-AMMWQAAAzkLsDE4k917.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=1024--1024__group1__$$M00$$00$$21$$CgqAy1d17M6AUBRoAAGWfUdODeU139.jpg"
-            ]
-        },{
-            id: "1456",
-            productImg: "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=800--800__group1__$$M00$$00$$35$$CgoSrFf92aqAZuS1AAD8G_1ckwE719.jpg",
-            title: "6人团-纯棉加绒加厚保暖背心吊带",
-            people: "6",
-            price: "9.80",
-            lack: "1",
-            userImg: [
-                "http://wx.qlogo.cn/mmopen/Q3auHgzwzM7wXR6Iw880XVEjjwF3gVp3MW8XksjDF1cQQ4YlGlZI4I3MKia6kkZHHvaRtl3licXdZTNIB34aSxZMtdDcqW7t40E8tP2y5y8wY/0",                
-                "http://wx.qlogo.cn/mmopen/XuNnRjxJia467j0VjmneIjOUyxdpvK7J0FWuunWhCpQibhR6lVoQmpYg1KQ2qQEic6N78sJGLfKMLQP0mJcFFoftWyM8G3iafTDe/0",
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgqAy1d17JuAEueWAAAemM4_igk664.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=200--200__group1__$$M00$$00$$21$$CgoSrFd112-AMMWQAAAzkLsDE4k917.jpg",                
-                "http://service.ipinbb.com:8080/ImageService/GetImage?imageName=1024--1024__group1__$$M00$$00$$21$$CgqAy1d17M6AUBRoAAGWfUdODeU139.jpg"
-            ]
-        }]
+        hidden: true,           // loading动画是否显示
+        tipShow: false,         // 判断是否还有更多数据
+        page: 1,               // 当前分页
+        nextPage: "",           // 分页信息
+        us: "",                 // 用户分享的ID，之前为了做分享统计什么的，现在没卵用
+        lists: []
     },
-    loadingChange: function () {
-        this.setData({
-            hidden: true
-        })
-    },
-    loadList: function(e) {
-        console.log("scroll bottom!!!")
-        this.setData({
-            hidden: false
-        })
 
-        var that = this
-        setTimeout(function () {
-            that.setData({
-                hidden: true
-            })
-        }, 1500)
+    onLoad: function(e) {
+        var listArr = [],
+            self = this,
+            page = self.data.page,
+            nextpage = self.data.nextpage,
+            us = self.data.us;
+        
+        // 显示加载动画;
+        self.setData({
+             hidden: false,
+        });
+
+        // 发送请求
+        wx.request({
+            url: "http://service.ipinbb.com:8080/dispatcherService/getAttendGroupList",
+            data: {
+                page: page,
+                nextpage: nextpage,
+                us: us
+            },
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function(res){
+
+               var nextpage = res.data.nextPage;
+
+                console.log(res);
+                // 关闭加载动画并将请求回来的数据添加到data中
+                self.setData({
+                    hidden: true,
+                    page: page + 1,
+                    nextpage: nextpage,
+                    lists: self.dataRead(res.data.lst)
+                })
+            },
+            file: function(e){
+                console.log("网络错误!!");
+            }
+        });
+    },
+
+    // 上拉加载跟多数据
+    loadList: function(e) {
+        var listArr = [],
+            self = this;
+
+        // 判断是否存在更多数据
+        if(!self.tipShow) {
+            // 显示加载动画;
+            self.setData({
+                hidden: false,
+            });
+
+            var page = Number(self.data.page);
+            console.log(page);
+            
+            // 发送请求
+            wx.request({
+                url: "http://service.ipinbb.com:8080/dispatcherService/getAttendGroupList",
+                data: {
+                    page: page,
+                    nextpage: self.data.nextpage,
+                    us: self.data.us
+                },
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                success: function(res){
+
+                    
+                    var nextpage = JSON.stringify(res.data.nextPage);
+
+                    if(res.data != null) {
+                        // 将请求回来的数据添加到data中
+                        self.setData({
+                            hidden: true,
+                            page: page + 1,
+                            nextpage: nextpage,
+                            lists: self.data.lists.concat(self.dataRead(res.data.lst))
+                        })
+                    } else {
+                        self.setData({
+                            hidden: true,
+                            tipShow: true
+                        });
+                    }
+                },
+                file: function(e){
+                    console.log("网络错误!!");
+                }
+            });
+        }
+    },
+
+    // 对数据进行提取
+    dataRead: function(data) {
+        // 用于存放提取后的数据
+        var dataArr = [];
+
+        data.forEach(function(item) {
+            // 用于存放单条团数据
+            var groupData = {};
+
+            groupData.groupId = item.groupId;
+            groupData.goodsImg = item.goodsImg;
+            groupData.goodsTitle = item.goodsTitle;
+            groupData.groupSize = item.groupSize;
+            groupData.groupPrice = item.groupViewPrice;
+            groupData.lessNum = item.lessNum;
+            groupData.groupEndTime = item.restTime;
+            groupData.userLst = item.userLst;
+
+            dataArr.push(groupData);
+        });
+
+        return dataArr;
     }
 })
